9880acc260304bfe51449bea2cd58bb890b311e1
Angela Williams <lambda_school_loaner_97@Lambda-School-Loaner-97s-MacBook-Pro.local>
2020-03-14 16:43:09 -0700

    Added redux dependencies, started setting up reducers/actions

---
+++ friends/src/actions/index.js
@@ -0,0 +1,29 @@
+import axios from 'axios';
+// import { axiosWithAuth } from '';
+
+export const LOGIN_START = 'LOGIN_START';
+export const FETCHING_FRIENDS = 'FETCHING_FRIENDS';
+export const FETCHING_FRIENDS_SUCCESS = 'FETCH_FRIENDS_SUCCESS';
+export const FETCH_FRIENDS_FAIL = 'FETCH_FRIENDS_FAIL';
+
+export const login = creds => dispatch => {
+    dispatch({ type: LOGIN_START });
+    return axios
+    .post('http://localhost:5000/api/login', creds)
+    .then(res => {
+        localStorage.setItem('userToken', res.data.payload);
+        return true;
+    })
+    .catch(err => {
+        console.error(err.response);
+    })
+};
+
+export const getFriends = () => {
+    dispatch({ type: FETCHING_FRIENDS });
+    axiosWithAuth()
+      .get('/friends')
+      .then(res => {
+          console.log(res);
+      })
+};
